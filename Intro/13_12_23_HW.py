851c645d2bbb39e2e7baa8b070cf8654fa0d08f6
Admin <Admin@LAPTOP>
2024-01-12 08:10:05 +0200

    Basics + Opers + Func

---
+++ Intro/13_12_23_HW.py
@@ -0,0 +1,30 @@
+def input_positive_number(prompt):
+    # Функція для введення позитивного числа.
+    while True:
+        try:
+            number = float(input(prompt))
+            if number > 0:
+                return number
+            else:
+                print("The number must be positive")
+        except ValueError:
+            print("Please enter a valid number")
+
+def main():
+    # Введення першого позитивного числа
+    x = input_positive_number("Enter x = ")
+
+    # Введення другого позитивного числа, яке відрізняється від першого
+    while True:
+        y = input_positive_number("Enter y = ")
+        if y != x:
+            break
+        else:
+            print("The numbers should be different")
+
+    # Обчислення та виведення суми
+    print(f"{x} + {y} = {x + y}")
+
+
+if __name__ == "__main__":
+    main()
